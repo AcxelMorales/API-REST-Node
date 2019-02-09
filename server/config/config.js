8e231ee3cb5e69119efa79e969b4f65297fe8314
Acxel Morales <acxelmorales97@gmail.com>
2019-02-09 01:35:02 -0600

    Serve

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let url
 if (process.env.NODE_ENV === 'dev') {
     url = 'mongodb://localhost:27017/cafe'
 } else {
-    url = 'mongodb://cafe-admin:Ca52341698RoCk_<dbpassword>@ds127825.mlab.com:27825/cafe'
+    url = 'mongodb://cafe-admin:Ca52341698RoCk_@ds127825.mlab.com:27825/cafe'
 }
 
 process.env.URL_DB = url
